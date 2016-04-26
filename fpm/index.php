109e8180fe1af5018e6b086d49e1b5dbac69aa89
benoit <benoit.oyez@telecomnancy.net>
2016-04-26 23:44:27 +0200

    First page v1

--- fpm/index.php
+++ fpm/index.php
@@ -1,62 +1,48 @@
 ﻿<?php
 	$experiences = [
 		[
-			"job" => "De Octobre 2015 à aujourd'hui : Devops (CDI), au sein de Top Chrono Innovation, Paris, éditeur de Jogg.in, réseau social autour des activités du running",
+			"job" => "De Octobre 2015 à aujourd'hui : Devops (CDI), au sein de Top Chrono Innovation, Paris, éditeur de Jogg.in, réseau social des activités du running",
 			"missions" => [
 				[
 					"tasksList" => [
-						"Installation d'un gestionnaire de dépôt Git",
-						"Configuration d'une plateforme d'intégration continue",
-						"Stratégie de test de l'API de données",
-						"Installation, configuration de l'environnement de production",
-						"Mise en place d'une solution de backup",
-						"Déploiement multi hôtes",
-						"Intégration de l'agent et création de dashboards de monitoring",
-						"Stratégie d'architecture REST et communication des données aux clients",
-						"Caching de données de l'application"
+						"Environnement de développement virtualisé et tests fonctionnels",
+						"Gestionnaire de version Git et intégration continue",
+						"Installation et configuration du déploiement continu",
+						"Santé de la production: monitoring et backup",
+						"Architecture et structure de communication client-serveur"
 					],
 					"technologies" => [
-						"Behat",
-						"Gitlab",
-						"Docker",
-						"Datadog"
+						"Behat, Docker, Gitlab, Datadog"
 					]
 				]
 			]
 		],
 		[
-			"job" => "De Avril 2015 jusqu'à aujourd'hui : Lead Développeur (CDI), au sein de Devispresto, Paris, éditeur des sites web Devispresto.com et Habitatpresto.com de mise en relation particuliers-professionels dans le domaine du bâtiment",
+			"job" => "De Avril 2015 jusqu'à Octobre 2015 : Lead Développeur (CDI), au sein de Devispresto, Paris, sites Habitatpresto.com, devis dans le bâtiment",
 			"missions" => [
 				[
 					"tasksList" => [
-						"Maintenance corrective du système Backoffice et site Frontoffice",
-						"Architecture et développement de plusieurs fonctionnalités majeures de l'administration Backoffice",
-						"Encadrement et formation de membres juniors de l'équipe sur l'administration Backoffice",
-						"Architecture et développement des applications mobiles iOS et Android",
-						"Refonte graphique du site principal, responsive design"
+						"Développement de fonctionnalités Backoffice",
+						"Encadrement de membres juniors",
+						"Développement des applications mobiles iOS et Android",
+						"Refonte graphique du site principal"
 					],
 					"technologies" => [
-						"PHP, Zend 1, Laravel",
-						"HTML, CSS, Less, Bootstrap",
-						"Javascript, jQuery",
-						"Objective C, Java Android"
+						"Zend 1, Laravel, Less, Bootstrap, jQuery, Objective C, Java Android"
 					]
 				]
 			]
 		],
 		[
-			"job" => "De Août 2014 jusqu'à Mars 2015 : Développeur web (CDI), au sein de Key Performance Group, Paris, agence de solutions marketing utilisant les nouvelles technologies",
+			"job" => "De Août 2014 jusqu'à Mars 2015 : Développeur web (CDI), au sein de Key Performance Group, Paris, agence de développement de jeux concours",
 			"missions" => [
 				[
 					"tasksList" => [
-						"Architecture et développement modulaire d'une plateforme Saas de création de jeux concours en ligne",
-						"Développement de sites événementiels jeux concours sur mesure avec interface d'extraction de données"
+						"Plateforme SaaS de création de jeux concours en ligne",
+						"Sites événementiels jeux concours et interfaces d'extraction de données"
 					],
 					"technologies" => [
-						"PHP, Silex",
-						"MySQL",
-						"HTML, CSS",
-						"Javascript, jQuery"
+						"Silex, MySQL, jQuery"
 					]
 				]
 			]
@@ -66,16 +52,13 @@
 			"missions" => [
 				[
 					"tasksList" => [
-						"Conception et développement d'un portail CMS",
-						"Prise en main des technologies et formation des membres juniors de l'équipe",
-						"Développement d'un Backoffice API REST et interface d'administration",
+						"Conception et développement d'un site portail CMS",
+						"Suivi de membres juniors de l'équipe",
+						"Dév. API REST, Backoffices",
 						"Coordination client-serveur des échanges de données temps réel",
 					],
 					"technologies" => [
-						"PHP, Joomla!, Prestashop",
-						"MySQL",
-						"HTML, CSS",
-						"Javascript, jQuery"
+						"Joomla!, Prestashop, MySQL, jQuery"
 					]
 				],
 				[
@@ -83,11 +66,7 @@
 						"Conception, développement et maintenance évolutive d'une quinzaine d'applications mobiles"
 					],
 					"technologies" => [
-						"C#",
-						"C++",
-						"Objective C",
-						"HTML, CSS",
-						"Javascript, jQuery"
+						"C# Windows Phone, C++ Qt, Objective C, jQuery"
 					]
 				]
 			]
@@ -95,141 +74,85 @@
 	];
 	$talentsList = [
 		[
-			"title" => "Compétences informatiques",
+			"title" => "Compétences",
 			"list" => [
 				[
 					"note" => 4,
-					"name" => "Frontend Web + Responsive design"
+					"name" => "Dév. Back, Front, mobile"
 				],
 				[
-					"note" => 4,
-					"name" => "Développement Backend"
+					"note" => 3,
+					"name" => "Paradigmes de prog."
 				],
 				[
-					"note" => 4,
-					"name" => "Développement mobile"
-				],
-				[
-					"note" => 4,
-					"name" => "Format de flux de données: XML, JSON ..."
+					"note" => 3,
+					"name" => "Architecture de services"
 				],
                 [
                     "note" => 3,
-                    "name" => "Environnements virtualisés"
-                ],
-                [
-                    "note" => 3,
-                    "name" => "Base de données NoSQL/Big Data"
+                    "name" => "Automatisation, CI, CD"
                 ],
 				[
 					"note" => 3,
-					"name" => "Structure Webservices, API, REST ..."
-				],
-				[
-					"note" => 3,
-					"name" => "Base de données relationnelles"
-				],
-				[
-					"note" => 3,
-					"name" => "Prog. orientée objets: PHP, Java, C++, C# ..."
-				],
-				[
-					"note" => 3,
-					"name" => "Programmation parallèle"
-				],
-				[
-					"note" => 3,
-					"name" => "Programmation événementielle"
-				],
-				[
-					"note" => 3,
-					"name" => "Conception, architecture, patterns"
-				],
-                [
-                    "note" => 2,
-                    "name" => "Intégration et déploiement continus"
-                ],
-				[
-					"note" => 2,
-					"name" => "Optimisation de base de données"
+					"name" => "Base de données"
 				],
 				[
 					"note" => 2,
-					"name" => "Découplage structurel, architecture de services"
-				],
-				[
-					"note" => 2,
-					"name" => "Paramétrage, administration réseau"
-				],
-                [
-                    "note" => 1,
-                    "name" => "Monitoring de serveurs"
-                ],
-				[
-					"note" => 1,
-					"name" => "Haut traffic"
+					"name" => "Gestion de production"
 				]
 			]
 		],
 		[
-			"title" => "Outils de prédilection",
+			"title" => "Outils",
 			"list" => [
-				[
-					"note" => 4,
-					"name" => "GNU/Linux"
-				],
 				[
 					"note" => 4,
 					"name" => "IDE développement"
 				],
-				[
-					"note" => 4,
-					"name" => "Outils de gestion de base de données"
-				],
                 [
                     "note" => 3,
-                    "name" => "Virtualisation containers: Docker"
+                    "name" => "Virtualisation et containers"
                 ],
                 [
                     "note" => 3,
-                    "name" => "Versioning: SVN, Git, Github, Gitlab"
+                    "name" => "Plateformes de versioning"
                 ],
-				[
-					"note" => 3,
-					"name" => "Librairies JS: jQuery, Enyo"
-				],
-				[
-					"note" => 3,
-					"name" => "Bootstrap, structure en grille, responsive design"
-				],
 				[
 					"note" => 3,
 					"name" => "Apache/Nginx, serveurs Web"
 				],
 				[
 					"note" => 3,
-					"name" => "Frameworks PHP: Laravel, Symfony, Zend"
+					"name" => "Frameworks PHP"
+				],
+				[
+					"note" => 3,
+					"name" => "Responsive design, grille"
+				],
+				[
+					"note" => 2,
+					"name" => "Réseau"
 				],
 				[
 					"note" => 2,
 					"name" => "Compilateurs de CSS, Less"
-				],
-				[
-					"note" => 1,
-					"name" => "Suites bureautiques"
 				]
 			]
 		],
 		[
-			"title" => "Compétences, méthodes et suivi/gestion de projets",
+			"title" => "Méthodes et organisation",
 			"list" => [
                 [
                     "note" => 4,
-                    "name" => "Autoformation, montée en compétence"
+                    "name" => "Auto-formation"
                 ],
 				[
 					"note" => 3,
-					"name" => "Rédaction de documents"
+					"name" => "Rédaction de doc."
+				],
+				[
+					"note" => 2,
+					"name" => "Présentation, réunion"
 				],
 				[
 					"note" => 2,
@@ -237,19 +160,11 @@
 				],
 				[
 					"note" => 2,
-					"name" => "Gestion de bugs / suivi de projets"
-				],
-				[
-					"note" => 1,
-					"name" => "Méthode UML"
+					"name" => "Gestion de bugs"
 				],
 				[
 					"note" => 1,
 					"name" => "Base de connaissances"
-				],
-				[
-					"note" => 1,
-					"name" => "Réseaux sociaux d'entreprise"
 				]
 			]
 		]
@@ -276,74 +191,58 @@
 			</div>
 		</div>
 		<div class="container" id="title">Software Craftsman</div>
-		<div class="container"></div>
+		<div class="container" id="quote"><div id="quote-block"><blockquote>Donner plus de valeur au développement, plus tôt</blockquote></div></div>
 		<div class="container">
-			<div class="section" id="experiences">
-				<div class="title">Expériences choisies</div>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				<div id="experiences">
+					<div class="section" id="experiences">
+						<div class="title">Expériences choisies</div>
+					</div>
+					<table class="table">
+						<?php foreach($experiences as $experience) : ?>
+							<tr>
+								<td colspan="3" class="job">
+									<?= $experience['job']; ?>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<?php foreach($experience["missions"] as $mission) : ?>
+										<div>
+											<strong>Tâches</strong>
+											<ul class="tasksList">
+												<?php foreach($mission["tasksList"] as $task) : ?>
+													<li><?= $task ?></li>
+												<?php endforeach; ?>
+											</ul>
+										</div>
+										<div>
+											<strong>Technologies notables</strong>
+											<ul class="technologies">
+												<?php foreach($mission["technologies"] as $technology) : ?>
+													<li><?= $technology ?></li>
+												<?php endforeach; ?>
+											</ul>
+										</div>
+									<?php endforeach; ?>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+					</table>
+				</div>
 			</div>
-			<table class="table">
-			<?php foreach($experiences as $experience) : ?>
-                <tr>
-                    <td colspan="3" class="job">
-                        <?= $experience['job']; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <?php foreach($experience["missions"] as $mission) : ?>
-                        <div>
-                            <strong>Tâches</strong>
-                            <ul class="tasksList">
-                                <?php foreach($mission["tasksList"] as $task) : ?>
-                                <li><?= $task ?></li>
-                                <?php endforeach; ?>
-                            </ul>
-						</div>
-						<div>
-                            <strong>Technologies</strong>
-                            <ul class="technologies">
-                                <?php foreach($mission["technologies"] as $technology) : ?>
-                                <li><?= $technology ?></li>
-                                <?php endforeach; ?>
-                            </ul>
-                        </div>
-                        <?php endforeach; ?>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-			</table>
-		</div>
-		<div class="container">
-			<div class="section" id="formation">
-				<div class="title">Formation</div>
-				<table class="table">
-					<tr>
-						<td>2007-2010</td>
-						<td><strong>Diplôme d'ingénieur informatique</strong> généraliste à Télécom Nancy (anciennement ESIAL), coloration Ingénierie Logicielle</td>
-					</tr>
-					<tr>
-						<td>2005-2007</td>
-						<td><strong>Classes Préparatoires aux Grandes &Eacute;coles section MP,</strong> lycée Jean Bart, Dunkerque</td>
-					</tr>
-				</table>
-				<div>Pratique de l'anglais (lu et écrit) quotidienne, pratique courante de l'anglais oral, score de 870 au TOEIC</div>
-			</div>
-		</div>
-		<?php foreach($talentsList as $talents) : ?>
-		<div class="container">
-			<div class="section talent-list">
-				<div class="title"><?= $talents['title']; ?></div>
-				<div class="list">
-					<?php
-					$index = 0;					
-					foreach($talents['list'] as $talent) : 
-						if($index % 3 == 0) :
-						?>
-						<div class="row-table">
-						<?php endif; ?>
-						<div class="col-md-4 col-sm-4 col-xs-4 talent-block">
-							<div class="talent-container">	
-								<div class="stars"><!--
+			<div class="col-md-4 col-sm-4 col-xs-4">
+				<div id="talents">
+					<?php foreach($talentsList as $talents) : ?>
+						<div class="section talent-list">
+							<div class="title"><?= $talents['title']; ?></div>
+							<div class="list">
+								<div class="row-table">
+								<?php
+								foreach($talents['list'] as $talent) : ?>
+									<div class="col-md-6 col-sm-6 col-xs-6 talent-block">
+										<div class="talent-container">
+											<div class="stars"><!--
 								<?php for($i = 0; $i < $talent['note']; $i++) : ?>
 									--><img class="star" src="/images/star_complete.png"/><!--
 								<?php endfor; ?>
@@ -351,40 +250,47 @@
 									--><img class="star" src="/images/star_empty.png"/><!--
 								<?php endfor; ?>
 								--></div>
-								<div class="talent"><?= $talent['name'];?></div>
+											<div class="talent"><?= $talent['name'];?></div>
+										</div>
+									</div>
+								<?php
+								endforeach;
+								?>
+								</div>
 							</div>
 						</div>
-						<?php
-						if($index % 3 == 2 || $index == count($talents['list']) - 1) :
-						?>
-						</div>
-						<?php endif;
-					$index++;
-					endforeach; 
-					?>
+					<?php endforeach; ?>
+				</div>
+				<div id="formation" class="section">
+					<div class="title">Formation</div>
+					<table class="table">
+						<tr>
+							<td>2007-2010</td>
+							<td><strong>Diplôme d'ingénieur informatique</strong> généraliste à Télécom Nancy (anciennement ESIAL), coloration Ingénierie Logicielle</td>
+						</tr>
+						<tr>
+							<td>2005-2007</td>
+							<td><strong>CPGE section MP,</strong> lycée Jean Bart, Dunkerque</td>
+						</tr>
+					</table>
+					<div class="title">Anglais</div>
+					<table class="table">
+						<tr>
+							<td>lu et écrit, pratique quotidienne</td>
+						</tr>
+						<tr>
+							<td>oral, pratique courante</td>
+						</tr>
+						<tr>
+							<td>Score de 870 au TOEIC</td>
+						</tr>
+					</table>
 				</div>
 			</div>
 		</div>
-		<?php endforeach; ?>
+		<div class="page-break"></div>
 		<div class="container">
-			<div class="section" id="info_complement">
-				<div class="title">Informations complémentaires</div>
-			</div>
-			<table class="table">
-				<tr>
-					<td>
-						2008-2009
-					</td>
-					<td>
-						Responsable Animation d'<i>Anim'Est</i>, convention annuelle sur la culture japonaise (3700 visiteurs sur un weekend, bufget de 35 000€)
-					</td>
-				</tr>
-			</table>
-			<h3>Centres d'intérêt</h3>
-			<div>Veille technique, assister à divers Meetup informatique</div>
-			<div>Jeux de société, jeux de cartes, jeux vidéo, jeux de rôles</div>
-			<div>Théâtre d'improvisation, cinéma, musée, lecture</div>
-			<div>Course à pied, danses traditionnelles, rock à 4 temps</div>
+			TEST
 		</div>
 	</body>
 </html>
